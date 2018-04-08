ccd4caad63f205de6a14fa95fb5ad3293d2185dc
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-08 20:41:02 +0200

    refactor: Replace Router with App component in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Router from './components/Router';
+import App from './components/App';
 import './index.css';
 
-render(<Router />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));

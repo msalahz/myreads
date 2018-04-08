5ead16282c59c49789df6aa326da645cac61f021
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-09 01:48:23 +0200

    refactor: Rename ListBooks to BookList & use new components

---
+++ src/components/Book/BookList.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { array, func, bool } from 'prop-types';
+import Bookshelf from './Bookshelf';
+import BookshelfLoadingBlacholder from './BookshelfLoadingPlacholder';
+
+const bookshelfBooks = shelf => books =>
+  books.filter(book => book.shelf === shelf);
+const currentlyReadingBooks = bookshelfBooks('currentlyReading');
+const wantToReadBooks = bookshelfBooks('wantToRead');
+const readBooks = bookshelfBooks('read');
+
+const BookList = ({ loading, books, updateBookshelf }) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <div className="list-books-content">
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <BookshelfLoadingBlacholder loading={loading}>
+            <Bookshelf
+              loading={loading}
+              books={currentlyReadingBooks(books)}
+              updateBookshelf={updateBookshelf}
+            />
+          </BookshelfLoadingBlacholder>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Want to Read</h2>
+          <BookshelfLoadingBlacholder loading={loading}>
+            <Bookshelf
+              loading={loading}
+              books={wantToReadBooks(books)}
+              updateBookshelf={updateBookshelf}
+            />
+          </BookshelfLoadingBlacholder>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Read</h2>
+          <BookshelfLoadingBlacholder loading={loading}>
+            <Bookshelf
+              loading={loading}
+              books={readBooks(books)}
+              updateBookshelf={updateBookshelf}
+            />
+          </BookshelfLoadingBlacholder>
+        </div>
+      </div>
+    </div>
+    <div className="open-search">
+      <Link to="/search">Add a book</Link>
+    </div>
+  </div>
+);
+
+BookList.propTypes = {
+  books: array.isRequired,
+  loading: bool.isRequired,
+  updateBookshelf: func.isRequired
+};
+
+export default BookList;

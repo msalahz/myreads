483893c7b9e0506d1b5e741931fff639d54c7d58
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-09 01:39:40 +0200

    feat: Create BookshelfLoadingPlaceholder component

---
+++ src/components/Book/BookshelfLoadingPlacholder.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BookCardLoadingPlaceholder from './BookCardLoadingPlacholder';
+
+const renderLoading = () => (
+  <div className="bookshelf-books">
+    <ol className="books-grid">
+      <BookCardLoadingPlaceholder />
+      <BookCardLoadingPlaceholder />
+      <BookCardLoadingPlaceholder />
+    </ol>
+  </div>
+);
+const BookshelfLoadingPlaceholder = ({ loading, children }) =>
+  loading ? renderLoading() : children;
+
+export default BookshelfLoadingPlaceholder;

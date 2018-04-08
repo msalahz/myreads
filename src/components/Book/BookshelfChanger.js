47fbd414802c75c8ceac2679faf185bb059e0ca3
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-09 01:34:48 +0200

    feat: Create BookshelfChanger component

---
+++ src/components/Book/BookshelfChanger.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { object, func } from 'prop-types';
+
+class BookshelfChanger extends React.Component {
+  handleChange = e => {
+    const { book, updateBookshelf } = this.props;
+    updateBookshelf(book, e.target.value);
+  };
+
+  render() {
+    const { book } = this.props;
+
+    return (
+      <div className="book-shelf-changer">
+        <select value={book.shelf} onChange={this.handleChange}>
+          <option value="none" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+BookshelfChanger.propTypes = {
+  book: object.isRequired,
+  updateBookshelf: func.isRequired
+};
+
+export default BookshelfChanger;

cc7221cfc0fe1748457b7baa4385376910247d02
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-10 16:51:33 +0200

    refactor: Show Book Card Loading Placeholders based on screen width

--- src/components/Book/BookshelfLoadingPlaceholder.js
+++ src/components/Book/BookshelfLoadingPlaceholder.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import BookCardLoadingPlaceholder from './BookCardLoadingPlaceholder';
 
+const count = Math.floor(window.innerWidth / 180);
+const randomKey = () => Math.floor(Math.random() * 10000);
+
 const renderLoading = () => (
   <div className="bookshelf-books">
     <ol className="books-grid">
-      <BookCardLoadingPlaceholder />
-      <BookCardLoadingPlaceholder />
-      <BookCardLoadingPlaceholder />
+      {new Array(count)
+        .fill(0)
+        .map(() => <BookCardLoadingPlaceholder key={randomKey()} />)}
     </ol>
   </div>
 );

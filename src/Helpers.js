4495106a50575323748f4baa6b07a72bb985a165
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-10 17:40:21 +0200

    refactor: Add makeCancelable func to helpers to allow cancelable promises

--- src/Helpers.js
+++ src/Helpers.js
@@ -6,3 +6,26 @@ export const currentlyReadingBooks = bookshelfBooks('currentlyReading');
 export const wantToReadBooks = bookshelfBooks('wantToRead');
 
 export const readBooks = bookshelfBooks('read');
+
+export const makeCancelable = promise => {
+  let hasCanceled_ = false;
+
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promise
+      .then(
+        val =>
+          hasCanceled_ ? reject(new Error('Request canceled')) : resolve(val)
+      )
+      .catch(
+        error =>
+          hasCanceled_ ? reject(new Error('Request canceled')) : reject(error)
+      );
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel() {
+      hasCanceled_ = true;
+    }
+  };
+};

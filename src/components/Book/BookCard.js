51feccee3c9e8e8b9cf250b2836dcde1f243154b
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-11 15:27:46 +0200

    refactor: Move BookCard inline styles separated from the code

--- src/components/Book/BookCard.js
+++ src/components/Book/BookCard.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { object, func } from 'prop-types';
 import BookshelfChanger from './BookshelfChanger';
 
+const style = book => ({
+  width: 128,
+  height: 193,
+  backgroundImage: `url("${book &&
+    book.imageLinks &&
+    book.imageLinks.thumbnail}")`
+});
+
 const BookCard = ({ book, updateBookShelf }) => (
   <li>
     <div className="book">
       <div className="book-top">
-        <div
-          className="book-cover"
-          style={{
-            width: 128,
-            height: 193,
-            backgroundImage: `url("${book &&
-              book.imageLinks &&
-              book.imageLinks.thumbnail}")`
-          }}
-        />
+        <div className="book-cover" style={style(book)} />
         <BookshelfChanger book={book} updateBookShelf={updateBookShelf} />
       </div>
       <div className="book-title">{book.title}</div>
